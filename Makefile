093fb892302c0785091e76247a42629712dc4fa8
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2022-06-15 09:19:49 +0200

    fixed makefile and bug

--- Makefile
+++ Makefile
@@ -6,67 +6,61 @@
 #    By: dyeboa <dyeboa@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2021/10/26 14:06:32 by dyeboa        #+#    #+#                  #
-#    Updated: 2022/06/10 18:13:58 by dyeboa        ########   odam.nl          #
+#    Updated: 2022/06/15 09:15:45 by dyeboa        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 # Program name
-NAME	= push_swap
-CC = gcc
+NAME		= 	push_swap
+CC 			= 	gcc
 
-CFLAGS = -Wall -Wextra -Werror -I ${INCLUDE}
-INCLUDE = ./include
+CFLAGS 		= 	-Wall -Wextra -Werror
+INCLUDE 	= 	./include
+VPATH 		= 	src
 
+SRC_DIR		=	src
+SRC_FILES 	=	input_check.c\
+				main.c\
+				list.c\
+				sort.c\
+				commands.c\
+				parser.c\
+				sort_small.c\
+				sort_utils.c
 
-SRC_DIR	=	src
-SRC_FILES =	input_check.c\
-			main.c\
-			list.c\
-			sort.c\
-			commands.c\
-			parser.c\
-			sort_small.c\
-			sort_utils.c
+HEADER_FILES = 	push_swap.h
 
-HEADER_FILES = 	push_swap.h \
-			
 # object files
-OBJ_DIR	=	obj
-OBJ 	= $(SRC_FILES:%.c=$(OBJ_DIR)/%.o)
+OBJ_DIR = obj
+OBJ_FILES = $(SRC_FILES:%.c=$(OBJ_DIR)/%.o)
 
 # Libft
-LIBFT_DIR = libft
+LIBFT = libft.a
 
+# Default build
+all: $(OBJ_DIR) $(LIBFT) $(NAME)
 
-
-# all
-all: $(NAME)
-
-.c.o:
-	${CC} ${CFLAGS} -I$(INCLUDE) -c $< -o ${<:.c=.o}
-			
 # obj dir
-${OBJ_DIR}:
-	mkdir -p ${OBJ_DIR}
+${OBJ_DIR}	:
+				mkdir -p ${OBJ_DIR}
 
 # Libft
-$(LIBFT):
-	make -C libft
+$(LIBFT)	:
+				make -C libft
+# Build 
+${NAME}		: 	${OBJ_FILES} 
+				$(CC) $(CFLAGS) $^ libft/$(LIBFT) -o $@
 
-# Build executable
-${NAME}: ${OBJ_FILES} 
-	$(CC) $(CFLAGS) $^ libft/$(LIBFT) -o $@
+$(OBJ_FILES): 	$(OBJ_DIR)/%.o : %.c $(HEADER_FILES)
+				$(CC) $(CFLAGS) -c $< -o $@
 
-$(OBJ_FILES): $(OBJ_DIR)/%.o : %.c $(HEADER_FILES)
-	$(CC) $(CFLAGS) -c $< -o $@
-	
-clean:
-	rm -rf ${OBJ_DIR}
-	${MAKE} fclean -C ${LIB_DIR}
+clean		:
+				rm -rf ${OBJ_DIR}
+				make fclean -C libft/
 
-fclean:	clean
-	rm -rf ${NAME}
+fclean		:	clean
+				rm -rf ${NAME}
 
-re : fclean all
+re 			: 	fclean all
 
-.PHONY : clean fclean all 
+.PHONY 		: 	clean fclean all 
---
+++ src/push_swap.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   push_swap.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dyeboa <dyeboa@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/06/07 19:08:39 by dyeboa        #+#    #+#                 */
+/*   Updated: 2022/06/15 09:07:37 by dyeboa        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "../libft/libft.h"
+
+typedef struct node
+{
+	int			i;
+	struct node	*previous;
+	struct node	*next;
+}	t_stack;
+
+//main.c
+void	free_message_exit(char **mat);
+void	free_arr(char **arg);
+void	free_list(t_stack *node);
+void	printlist(t_stack *a);
+
+//parser
+char	**fill_array(int argc, char **argv);
+char	**get_inp(int argc, char **argv);
+
+//check
+int		ft_error(char **arg);
+int		ft_isnumber(char *arg);
+int		ft_dupl(char **arg);
+int		ft_isint(char *arg);
+int		check_sorted(t_stack **stack);
+
+//list.c
+void	listadd_back(t_stack **stack, t_stack *current);
+int		list_len(t_stack *stack);
+t_stack	*list_last(t_stack *stack);
+t_stack	*list_new(int num);
+
+//sort.c
+void	sort(t_stack **a, t_stack **b);
+void	indexsort(t_stack **a, t_stack **b);
+void	radix(t_stack **a, t_stack **b);
+void	index_numbers(t_stack **radix, t_stack *origin);
+void	sort_many(t_stack **a, t_stack **b);
+
+//sort_utils.c
+int		maxnumber(t_stack *a);
+int		m(t_stack *k);
+int		minnumber(t_stack *k);
+int		sm(t_stack *k);
+
+//sort_small.c
+void	two_numbers(t_stack **a);
+void	three_numbers(t_stack **a, int max);
+void	four(t_stack **a, int min, int max);
+void	five(t_stack **a, int min);
+
+//commands.c
+void	pushstack(t_stack **dest, t_stack **src, char stack);
+void	rotate(t_stack **stack);
+void	reverse_rotate(t_stack **stack);
+void	swap(t_stack **stack, char stackchar);
+#endif
